25de05d43941a35129a37e98ccdc54f7c3af34dc
Jaime OGrady <27785640+jaimeogr@users.noreply.github.com>
2024-09-07 16:04:28 -0300

    created cheat sheet

---
+++ cheat_sheet.ps1
@@ -0,0 +1,7 @@
+# Kaggle
+
+kaggle competitions download -c house-prices-advanced-regression-techniques -p ./data_files
+
+Expand-Archive -Path ./datafiles/house-prices-advanced-regression-techniques.zip -DestinationPath ./data_files -Force
+
+Remove-Item ./data_files/house-prices-advanced-regression-techniques.zip
